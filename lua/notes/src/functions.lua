2d2aa5aca9546f31517a01ef24e9c7110f07f96b
Filipe Santos <filipesantosdev@gmail.com>
2025-01-12 02:01:42 +0000

    dev

--- lua/notes/src/functions.lua
+++ lua/notes/src/functions.lua
@@ -97,6 +97,8 @@ M.update = function(_oldPath, newType, dont_update_todos_md, newPath, dontOpenBu
         utils.update_first_line(_oldPath, newType);
     end
     vim.print(newPath)
+    vim.print(utils.get_location_from_type(newType))
+    vim.print(vim.fn.fnamemodify(_oldPath, "%:t"))
     newPath = newPath or utils.get_location_from_type(newType) .. "/" .. vim.fn.fnamemodify(_oldPath, "%:t");
     vim.print(newPath)
 
